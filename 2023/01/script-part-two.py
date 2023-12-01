dcc9de58eb2992802ea5e11b633c0d46bfd2af7e
Amine <74571889+aminenaim@users.noreply.github.com>
2023-12-01 15:31:53 +0000

    Add day 01 challenge

---
+++ 2023/01/script-part-two.py
@@ -0,0 +1,44 @@
+"""
+Advent of Code
+Day 1 - Trebuchet?! : https://adventofcode.com/2023/day/1
+Part Two
+"""
+import re
+
+def compute_calibration_value(line: str):
+    """
+    Given a line, returns the calibration value 
+    (first and last digit to form a two-digit number)
+    """
+    # filters out digits from the line
+    digits = re.sub(pattern='[^0-9]', repl='', string=line)
+
+    # form a two(digit number using first and last digit
+    calibration_value = int(f'{digits[0]}{digits[-1]}')
+
+    return calibration_value
+
+def replace_spelled_digits(line: str):
+    """
+    Given a line, returns the line with spelled out digits replaced by their numeric values
+    """
+    spelled_out_digits = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    
+    for i, word in enumerate(spelled_out_digits):
+        digit = i + 1
+        # replaces every spelled digit by its numeric value
+        line = line.replace(word, f'{word}{digit}{word}')
+
+    return line
+
+computed_puzzle = []
+
+with open('input.txt', 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        calibrationValue = compute_calibration_value(replace_spelled_digits(line))
+        computed_puzzle.append(calibrationValue)
+
+answer = sum(computed_puzzle)        
+
+print(answer)
